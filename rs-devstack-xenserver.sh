189689dd868796a2dd783fa9629ddb965c388de1
Mate Lakat <mate.lakat@citrix.com>
2013-12-03 22:53:55 +0000

    Shrinking the root disk with rescue

--- rs-devstack-xenserver.sh
+++ rs-devstack-xenserver.sh
@@ -107,28 +107,24 @@ nova rescue "$VM_ID"
 
 wait_for_ssh "$VM_IP"
 
-cat << EOF
-To access the rescue machine:
+cat shrink-xvdb.sh | ssh -q \
+    -o BatchMode=yes -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null -i "$TEMPORARY_PRIVKEY" root@$VM_IP \
+    bash -s -- "$XENSERVER_PASSWORD" "$AUTHORIZED_KEYS"
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$TEMPORARY_PRIVKEY" root@$VM_IP
-EOF
-exit 0
-
-# Resize partition
-e2fsck -f /dev/xvdb1
-resize2fs /dev/xvdb1 4G
-NUMBER_OF_BLOCKS=$(tune2fs -l /dev/xvdb1 | grep "Block count" | tr -d " " | cut -d":" -f 2)
-sfdisk -d /dev/xvdb | sed -e "s,[0-9]\{8\},$NUMBER_OF_BLOCKS,g" | sfdisk /dev/xvdb
-partprobe /dev/xvdb
-sync
-halt -p
+sleep 5
 
 nova unrescue "$VM_ID"
 
+wait_for_ssh "$VM_IP"
 
+cat << EOF
+To access the shrinked machine:
 
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$TEMPORARY_PRIVKEY" root@$VM_IP
+EOF
 
-
+exit 0
 
 {
 cat << EOF
---
+++ shrink-xvdb.sh
@@ -0,0 +1,19 @@
+set -eux
+
+fsck -n /dev/xvdb1
+tune2fs -O ^has_journal /dev/xvdb1
+e2fsck -f /dev/xvdb1
+resize2fs /dev/xvdb1 4G
+
+# Number of 4k blocks
+NUMBER_OF_BLOCKS=$(tune2fs -l /dev/xvdb1 | grep "Block count" | tr -d " " | cut -d":" -f 2)
+
+# Convert them to 512 byte sectors
+SIZE_OF_PARTITION=$(expr $NUMBER_OF_BLOCKS * 8)
+
+sfdisk -d /dev/xvdb | sed -e "s,[0-9]\{8\},$SIZE_OF_PARTITION,g" | sfdisk /dev/xvdb
+partprobe /dev/xvdb
+tune2fs -j /dev/xvdb1
+
+sync 
+halt -p
