b8af572857beeeec2f0506077f774979561f9fde
Mate Lakat <mate.lakat@citrix.com>
2013-12-04 18:23:22 +0000

    Make sure network is up

--- prepare-to-firstboot.sh
+++ prepare-to-firstboot.sh
@@ -4,6 +4,10 @@ cat > /root/generate-firstboot.sh << EOF
 #!/bin/bash
 set -eux
 
+while ! ping -c 1 xenserver.org > /dev/null 2>&1; do
+    sleep 1
+done
+
 ADDRESS=\$(grep -m 1 "address" /etc/network/interfaces | sed -e 's,^ *,,g' | cut -d " " -f 2)
 NETMASK=\$(grep -m 1 "netmask" /etc/network/interfaces | sed -e 's,^ *,,g' | cut -d " " -f 2)
 GATEWAY=\$(grep -m 1 "gateway" /etc/network/interfaces | sed -e 's,^ *,,g' | cut -d " " -f 2)
