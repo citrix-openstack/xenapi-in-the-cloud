68f6160b0721ba9176504c851b2d3186f9cdf7f6
Mate Lakat <mate.lakat@citrix.com>
2013-12-05 13:26:41 +0000

    Fix leftover escaping

--- first-cloud-boot/xenserver-firstboot-access-dom0.sh
+++ first-cloud-boot/xenserver-firstboot-access-dom0.sh
@@ -7,6 +7,6 @@ done
 
 . /root/cloud-settings
 
-xe pif-introduce device=eth0 host-uuid=\$(xe host-list --minimal) mac=$MACADDRESS
-xe pif-reconfigure-ip uuid=\$(xe pif-list device=eth0 --minimal) mode=static IP=$ADDRESS netmask=$NETMASK gateway=$GATEWAY DNS=$NAMESERVERS
-xe host-management-reconfigure pif-uuid=\$(xe pif-list device=eth0 --minimal)
+xe pif-introduce device=eth0 host-uuid=$(xe host-list --minimal) mac=$MACADDRESS
+xe pif-reconfigure-ip uuid=$(xe pif-list device=eth0 --minimal) mode=static IP=$ADDRESS netmask=$NETMASK gateway=$GATEWAY DNS=$NAMESERVERS
+xe host-management-reconfigure pif-uuid=$(xe pif-list device=eth0 --minimal)
