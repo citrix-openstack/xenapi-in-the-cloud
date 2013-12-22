90214c21f6c93605fc59c660e7cea90163680be2
Mate Lakat <mate.lakat@citrix.com>
2013-12-22 10:10:00 +0000

    do not wait for ssh

--- get-ip-address-of-instance.sh
+++ get-ip-address-of-instance.sh
@@ -3,12 +3,6 @@ set -eu
 
 vm_name="$1"
 
-function wait_for_ssh() {
-    while ! echo "kk" | nc -w 1 "$VM_IP" 22 > /dev/null 2>&1; do
-            sleep 1
-    done
-}
-
 vm_id=$(nova list | grep "$vm_name" | tr -d " " | cut -d "|" -f 2)
 
 while true; do
@@ -20,5 +14,4 @@ while true; do
     fi
 done
 
-wait_for_ssh
 echo $VM_IP
